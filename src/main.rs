236662cc1c162c710a85bcc468ae575606a23cb7
rafliesa <rflisaa@gmail.com>
2025-05-04 09:00:16 +0700

    Payment Service Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use std::fmt::Error;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .compile(
-            &["proto/services.proto"],
-            &["proto"],
-        )?;
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
